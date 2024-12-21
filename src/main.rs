1bcf1fa508b95911ddb064666ce2088da63e0c74
Mateus Devino <mateus@mdevino.com>
2024-12-21 16:34:18 -0300

    Print HTTP request
    Signed-off-by: Mateus Devino <mateus@mdevino.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 const BIND: &str = "127.0.0.1:7878";
 
@@ -6,8 +9,19 @@ fn main() {
     let listener = TcpListener::bind(BIND).unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
