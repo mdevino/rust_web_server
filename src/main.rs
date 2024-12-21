91e2e957c534b40605903a577a565508e102219c
Mateus Devino <mateus@mdevino.com>
2024-12-21 16:56:45 -0300

    Hello response
    Signed-off-by: Mateus Devino <mateus@mdevino.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -23,8 +24,12 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
 
-    println!("REQUEST:\n{http_request:#?}\nRESPONSE:\n{response:#?}\n\n");
+    let response = format!("{status_line}\r\nContent=Length: {length}\r\n\r\n{contents}");
+    let response_lines: Vec<_> = response.lines().filter(|line| !line.is_empty()).collect();
+    println!("REQUEST:\n{http_request:#?}\nRESPONSE:\n{response_lines:#?}\n\n");
     stream.write_all(response.as_bytes()).unwrap();
 }
