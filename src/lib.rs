884648da4906e664279663082c88bf4fac4a8b88
Mateus Devino <mateus@mdevino.com>
2024-12-22 16:33:59 -0300

    Add channels for thread communication
    Signed-off-by: Mateus Devino <mateus@mdevino.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 use core::panic;
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub const BIND_ADDRESS: &str = "127.0.0.1:7878";
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Creates a new ThreadPoll.
@@ -20,19 +26,25 @@ impl ThreadPool {
             panic!("ThreadPoll size cannot be 0.");
         }
 
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, receiver.clone()));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -42,8 +54,10 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         println!("Creating worker {id}");
         Worker { id, thread }
