b83d97801a1a1d7fb822219f44184db9d4993363
Mateus Devino <mateus@mdevino.com>
2024-12-22 06:52:02 -0300

    Not found page
    Signed-off-by: Mateus Devino <mateus@mdevino.com>

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,25 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    if http_request[0] == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent=Length: {length}\r\n\r\n{contents}");
-    let response_lines: Vec<_> = response.lines().filter(|line| !line.is_empty()).collect();
-    println!("REQUEST:\n{http_request:#?}\nRESPONSE:\n{response_lines:#?}\n\n");
-    stream.write_all(response.as_bytes()).unwrap();
+        let response = format!("{status_line}\r\nContent=Length: {length}\r\n\r\n{contents}");
+
+        let response_lines: Vec<_> = response.lines().filter(|line| !line.is_empty()).collect();
+        println!("REQUEST:\n{http_request:#?}\nRESPONSE:\n{response_lines:#?}\n\n");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent=Length: {length}\r\n\r\n{contents}");
+
+        let response_lines: Vec<_> = response.lines().filter(|line| !line.is_empty()).collect();
+        println!("REQUEST:\n{http_request:#?}\nRESPONSE:\n{response_lines:#?}\n\n");
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
